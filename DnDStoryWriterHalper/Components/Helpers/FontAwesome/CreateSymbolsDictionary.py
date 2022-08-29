fb75a006ce5ed326d778d3a20a9b3fe7650d892c
OOjeser <boss.gavrilenko@bk.ru>
2022-08-29 23:30:44 +0500

    добавлен инструмент для работы с FontAwesome

---
+++ DnDStoryWriterHalper/Components/Helpers/FontAwesome/CreateSymbolsDictionary.py
@@ -0,0 +1,43 @@
+﻿# namespace DnDStoryWriterHalper.Components.FontAwesome;
+# public static class SymbolsContainer
+# {
+#     public static readonly string[] Symbil =
+#     {
+#         "",
+#         ""
+#     };
+# }
+# public enum Symbols : int
+# {
+#     Two = 0
+# }
+
+def to_camel_case(text):
+    if text[0].isdigit():
+        text = "Symbol " + text
+    if text == "lock":
+        text = "Symbol lock"
+    s = text.replace("-", " ").replace("_", " ")
+    s = s.split()
+    if len(text) == 0:
+        return text
+    return s[0] + ''.join(i.capitalize() for i in s[1:])
+    
+
+
+import json
+# use icons.json from metadata from ImaggeAwesome.zip
+file = open("icons.json")
+data = json.load(file)
+file.close()
+symbols = data.keys()
+enumString = "public enum Symbols : int{"
+i = 0
+for s in symbols:
+    enumString += "    " + to_camel_case(s) + " = 0x" + data[s]["unicode"] + ","
+    i += 1
+
+enumString += "}"
+
+open("symbols.cs", "w").write("namespace DnDStoryWriterHalper.Components.Helpers.FontAwesome;\n"+
+enumString)
---
+++ DnDStoryWriterHalper/Components/Helpers/FontAwesome/ImageAwesome.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using FontAwesome.WPF;
+using Wpf.Ui.Common;
+
+namespace DnDStoryWriterHalper.Components.Helpers.FontAwesome
+{
+    public class ImageAwesome
+    {
+        public static readonly DependencyProperty FontAwesomeProperty =
+            DependencyProperty.RegisterAttached(
+                "FontAwesome",
+                typeof(Symbols),
+                typeof(Label),
+                new UIPropertyMetadata(Symbols.Symbol0, (o, args) =>
+                {
+                    int unicodeValue = (int)(Symbols)args.NewValue;
+                    var tb = (Label)o;
+                    FontFamily[] fonts = new[]
+                    {
+                        new FontFamily(new Uri("pack://application:,,,/"), "./files/Font Awesome 6 Free-Solid-900.otf#Font Awesome 6 Free Solid"),
+                        new FontFamily(new Uri("pack://application:,,,/"), "./files/Font Awesome 6 Free-Regular-400.otf#Font Awesome 6 Free Regular"),
+                        new FontFamily(new Uri("pack://application:,,,/"), "./files/Font Awesome 6 Brands-Regular-400.otf#Font Awesome 6 Brands Regular")
+
+                    };
+                    foreach (var family in fonts)
+                    {
+                        var typefaces = family.GetTypefaces();
+                        foreach (Typeface typeface in typefaces)
+                        {
+                            typeface.TryGetGlyphTypeface(out var glyph);
+                            if (glyph != null && glyph.CharacterToGlyphMap.TryGetValue(unicodeValue, out var glyphIndex))
+                            {
+                                tb.FontFamily = family;
+                                break;
+                            }
+                        }
+                        tb.Content = Convert.ToChar(unicodeValue);
+
+                    }
+                    //tb.Content = Encoding.Unicode.GetString(BitConverter.GetBytes((int)(Symbols)args.NewValue));
+                    //tb.Content = ((int)(Symbols)args.NewValue).ToString("X");
+                }));
+        public static string GetFontAwesome(DependencyObject obj)
+        {
+            return (string)obj.GetValue(FontAwesomeProperty);
+        }
+
+        public static void SetFontAwesome(DependencyObject obj, Symbols value)
+        {
+            obj.SetValue(FontAwesomeProperty, value);
+        }
+    }
+}
